2e491a4a53cdbbb3b2605c7f9ee31c07a5b9b67d
brunownk <neckel.bw@gmail.com>
2021-06-16 23:53:24 -0300

    add subscribe button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image'
+
 import { SignInButton } from '../SignInButton';
+
+import Logo from '../../../public/images/logo.svg';
+
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src={Logo} alt="ig.news" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,38 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { SubscribeButton } from '~/components/SubscribeButton';
+import Avatar from '../../public/images/avatar.svg';
+import Clapping from '../../public/images/clapping.svg';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
    <>
      <Head>
-      <title>ig.news</title>
+      <title>Home | ig.news</title>
      </Head>
-     <h1>Hello world!</h1>
+
+     <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <div>
+          <Image src={Clapping} alt="" />
+          <span>Hey, welcome</span>
+        </div>
+        <h1>
+          News about <br />
+          the <span>React</span> world.
+        </h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+
+      <Image src={Avatar} alt="Girl coding" />
+     </main>
    </>
   )
 }
