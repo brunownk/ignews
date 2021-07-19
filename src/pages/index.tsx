608441a8d8c08692724933d5c2b0ff5134b317cf
Bruno Neckel <neckel.bw@gmail.com>
2021-07-19 19:24:57 -0300

    add stripe and SSG for subscribe price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,66 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 import { SubscribeButton } from '~/components/SubscribeButton';
+import { stripe } from '~/services/stripe';
 import Avatar from '../../public/images/avatar.svg';
 import Clapping from '../../public/images/clapping.svg';
 
 import styles from './home.module.scss';
 
-export default function Home() {
-  return (
-   <>
-     <Head>
-      <title>Home | ig.news</title>
-     </Head>
-
-     <main className={styles.contentContainer}>
-      <section className={styles.hero}>
-        <div>
-          <Image src={Clapping} alt="" />
-          <span>Hey, welcome</span>
-        </div>
-        <h1>
-          News about <br />
-          the <span>React</span> world.
-        </h1>
-        <p>
-          Get access to all the publications <br />
-          <span>for $9.90 month</span>
-        </p>
-        <SubscribeButton />
-      </section>
-
-      <Image src={Avatar} alt="Girl coding" />
-     </main>
-   </>
-  )
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
 }
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <div>
+            <Image src={Clapping} alt="" />
+            <span>Hey, welcome</span>
+          </div>
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <Image src={Avatar} alt="Girl coding" />
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JF42qJlQofMnDBY7B2jarpZ');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
