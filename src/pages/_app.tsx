ccfc30aeff69f9c124832f343d696cb58443e0ed
brunownk <neckel.bw@gmail.com>
2021-07-27 14:04:02 -0300

    test commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { AppProps } from 'next/app';
 import { Header } from '~/components/Header';
 
 import '~/styles/global.scss';
-
+// teste
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
